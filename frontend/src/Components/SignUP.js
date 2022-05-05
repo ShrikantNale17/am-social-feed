c796c309c5a5bd5a1ec1d66ecaf0cb425c682f60
apurvakirve <apurva.kirve@gmail.com>
2022-05-05 15:23:56 +0530

    done

--- frontend/src/Components/SignUP.js
+++ frontend/src/Components/SignUP.js
@@ -9,7 +9,7 @@ import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />; //returns something
 });
 
 const SignUP = () => {
