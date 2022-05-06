1fca80b52fec80ebd868e1feacafd962730246f0
apurvakirve <apurva.kirve@gmail.com>
2022-05-06 17:24:33 +0530

    q

--- frontend/src/Components/Feed.js
+++ frontend/src/Components/Feed.js
@@ -111,11 +111,11 @@ const Feed = (props) => {
       prevPosts.map((post) =>
         post._id === postID
           ? {
-            ...post,
-            likes: post.likes.includes(uid)
-              ? post.likes.filter((userID) => userID !== uid)
-              : [...post.likes, uid],
-          }
+              ...post,
+              likes: post.likes.includes(uid)
+                ? post.likes.filter((userID) => userID !== uid)
+                : [...post.likes, uid],
+            }
           : post
       )
     );
@@ -139,9 +139,9 @@ const Feed = (props) => {
         prevPosts.map((post) =>
           post._id === postID
             ? {
-              ...post,
-              comments: [...post.comments, { userID: uid, comment: comment }],
-            }
+                ...post,
+                comments: [...post.comments, { userID: uid, comment: comment }],
+              }
             : post
         )
       );
@@ -290,10 +290,11 @@ const Feed = (props) => {
                                 sx={{ bgcolor: red[500] }}
                                 aria-label="recipe"
                                 // src={each.user.image}
-                                src={`http://localhost:8080/${users.filter(
-                                  (user) => each.userID === user._id
-                                )[0].image
-                                  }`}
+                                src={`http://localhost:8080/${
+                                  users.filter(
+                                    (user) => each.userID === user._id
+                                  )[0].image
+                                }`}
                               >
                                 {/* {each.user?.firstname.charAt(0)} */}
                               </Avatar>
@@ -372,10 +373,7 @@ const Feed = (props) => {
                                     // placeholder="Add Comment"
                                     style={{ minWidth: 370, margin: "10px" }}
                                     value={comment}
-                                    onChange={(e) =>
-                                      setComment(e.target.value)
-
-                                    }
+                                    onChange={(e) => setComment(e.target.value)}
                                     fullWidth
                                   />
                                 </div>
@@ -427,11 +425,12 @@ const Feed = (props) => {
                                             fontSize: "15px",
                                           }}
                                           aria-label="recipe"
-                                          src={`http://localhost:8080/${users.filter(
-                                            (user) =>
-                                              each2.userID === user._id
-                                          )[0]?.image
-                                            }`}
+                                          src={`http://localhost:8080/${
+                                            users.filter(
+                                              (user) =>
+                                                each2.userID === user._id
+                                            )[0]?.image
+                                          }`}
                                         >
                                           {/* {each2.user.charAt(0)} */}
                                         </Avatar>
